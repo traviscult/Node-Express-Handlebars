3abc24471aba59e0228c4788c78cd77449d92ceb
traviscult <traviscult@gmail.com>
2020-05-14 16:47:56 -0400

    created selectall function in orm

--- config/orm.js
+++ config/orm.js
@@ -1,10 +1,69 @@
 const connection = require("../config/connection.js");
 
 
-selectAll()
+// Helper function for SQL syntax.
+// Let's say we want to pass 3 values into the mySQL query.
+// In order to write the query, we need 3 question marks.
+// The above helper function loops through and creates an array of question marks - ["?", "?", "?"] - and turns it into a string.
+// ["?", "?", "?"].toString() => "?,?,?";
+function printQuestionMarks(num) {
+    var arr = [];
 
-insertOne()
+    for (var i = 0; i < num; i++) {
+        arr.push("?");
+    }
 
-updateOne()
+    return arr.toString();
+}
+
+// Helper function to convert object key/value pairs to SQL syntax
+function objToSql(ob) {
+    var arr = [];
+
+    // loop through the keys and push the key/value as a string int arr
+    for (var key in ob) {
+        var value = ob[key];
+        // check to skip hidden properties
+        if (Object.hasOwnProperty.call(ob, key)) {
+            // if string with spaces, add quotations (Lana Del Grey => 'Lana Del Grey')
+            if (typeof value === "string" && value.indexOf(" ") >= 0) {
+                value = "'" + value + "'";
+            }
+            // e.g. {name: 'Lana Del Grey'} => ["name='Lana Del Grey'"]
+            // e.g. {sleepy: true} => ["sleepy=true"]
+            arr.push(key + "=" + value);
+        }
+    }
+
+    // translate array of strings to a single comma-separated string
+    return arr.toString();
+}
+
+const orm = {
+    selectAll: (tableInput, cb) => {
+        let queryString = `SELECT * FROM ${tableInput};`;
+        connection.query(queryString, (err, res) => {
+            if (err) {
+                throw err;
+            }
+            cb(res);
+        });
+    },
+    insertOne: (tableName, cols, vals, cb) = {
+        let queryString = `INSERT INTO ${tableName} (${cols.toString()}) VALUES (${printQuestionMarks(vals.length)});`;
+        connection.query(queryStatement, vals, (err, result) => {
+            if (err) throw err;
+            console.log("Sucesfully Added");
+            callback(result);
+        })
+
+    },
+
+    updateOne:
+
+}
+
+
+
 
 module.exports = orm;
--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -5,7 +5,16 @@ const router = express.Router();
 // Import the model (burgers.js) to use its database functions.
 const burgers = require("../models/burger.js");
 
-
+router.get("/", (req,res) => {
+    console.log("Router.Get is being called")
+    burgers.selectAll((data) =>{
+       const hbsobj = {
+           burgers: data
+       };
+       console.log(hbsobj)
+       res.render("index", hbsobj) 
+    });
+});
 //need to create router functions
 
 // Export routes for server.js to use.
